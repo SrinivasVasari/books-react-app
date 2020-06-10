07c594c11ee5b5df7fe5b894bdec4bd1ebd3806e
SrinivasV <thanmay4@gmail.com>
2020-06-10 19:13:13 +0530

    Updates

---
+++ precache-manifest.ad6148d43814ed3e8b3b3dada644f4ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87950e783a6a5956ea903ee26b48d57",
+    "url": "/books-react-app/index.html"
+  },
+  {
+    "revision": "328cdd34fc7671eb5922",
+    "url": "/books-react-app/static/css/main.7d85ceca.chunk.css"
+  },
+  {
+    "revision": "23439e5e7010d45251b3",
+    "url": "/books-react-app/static/js/2.013867ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/books-react-app/static/js/2.013867ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328cdd34fc7671eb5922",
+    "url": "/books-react-app/static/js/main.75f91c32.chunk.js"
+  },
+  {
+    "revision": "6ac2029e8481c95cee4e",
+    "url": "/books-react-app/static/js/runtime-main.8e70d1f2.js"
+  }
+]);
